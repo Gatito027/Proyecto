e99178d556a648e2b0ed745c21be7e99807856d6
Gatito027 <dddanieeel@hotmail.com>
2024-06-18 08:40:42 -0600

    0.1.f

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,6 +1,8 @@
 from django.db import connection
 from django.shortcuts import redirect, render
 from .models import *
+from datetime import datetime
+import random
 
 # Create your views here.
 def index(request):
@@ -20,27 +22,66 @@ def ListaProyectos(request, usuario):
     with connection.cursor() as cursor:
         cursor.callproc('BuscarRolByID', [usuario])
         rol = cursor.fetchone()[0]
-
         if rol == 1:
             cursor.callproc('ListaProfesoresproyecto', [])
             listaProfesores = cursor.fetchall()
-
-            cursor.callproc('BuscarProyectoImpartido', [usuario])
+            cursor.callproc('BuscarProyectos', [usuario])
             proyectos = cursor.fetchall()
         elif rol == 2:
             cursor.callproc('ListaProfesoresproyecto', [])
             listaProfesores = cursor.fetchall()
-
             cursor.callproc('BuscarProyectoImpartido', [usuario])
             proyectos = cursor.fetchall()
-
         else:
-            print("error")
-
+            return redirect('Error','Datos no validos')
     return render(request, 'pages/proyectos/ListaProyectos.html', {
         'rol': rol,
         'listaProfesores': listaProfesores,
-        'proyectos': proyectos    })
+        'proyectos': proyectos,
+        'usuario' : usuario })
+
+def generar_codigo_unico():
+    while True:
+        codigo = random.randint(-999999999, 999999999)
+        with connection.cursor() as cursor:
+            cursor.callproc('CodigosClase', [])
+            codigos_clase = cursor.fetchall()
+            codigos_existentes = {codigo_clase[0] for codigo_clase in codigos_clase}
+            if codigo not in codigos_existentes:
+                return codigo
+
+def crearProyecto(request,usuario):
+    nombre=request.POST['nombre']
+    materia=request.POST['materia']
+    descripcion=request.POST['descripcion']
+    fecha_inicio=request.POST['fecha_inicio']
+    fecha_fin=request.POST['fecha_fin']
+    ciclo_escolar=request.POST['ciclo_escolar']
+    color=request.POST['color']
+    codigo =generar_codigo_unico()
+    with connection.cursor() as cursor:
+        cursor.callproc('CrearProyectocoil', [
+            usuario,
+            str(nombre),
+            str(materia),
+            str(codigo),
+            str(descripcion),
+            datetime.strptime(fecha_inicio, '%Y-%m-%d').date(),
+            datetime.strptime(fecha_fin, '%Y-%m-%d').date(),
+            str(ciclo_escolar),
+            str(color)
+        ])
+        resultado = cursor.fetchone()[0]
+    if resultado == 'Proyecto creado exitosamente':
+        return redirect('IntroduccionCoil')
+    else:
+        return redirect('Error',resultado)
+    
+def UnirteProyecto(request, usuario):
+    print(request.POST)
+    codigo=request.POST['codigo']
+    terminos=request.POST['terminos']
+    redirect('Error','Prueba')
 
 def IntroduccionCoil(request):
     return render(request,'pages/Proyectos/IntroduccionCoil.html',{'enlace_activo': 'coil'})
@@ -89,6 +130,8 @@ def Fase4(request):
 def Fase5(request):
     return render(request, 'pages/FasesCoil/fase5.html', {'enlace_activo': 'tareas','enlace_activo1': 'fase5'})
 
+def Error(request,error):
+    return render(request, 'pages/error.html',{'error':error})
 
 
 # def articulos(request):
